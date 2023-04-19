b0638957899633ba84b76c32a1a6935f16300209
Angey22 <babandval@gmail.com>
2023-04-19 18:41:30 +0300

    Added video time in file '03-filter.js'

--- js-repeta_4-3/03-filter.js
+++ js-repeta_4-3/03-filter.js
@@ -1,3 +1,5 @@
+//! Видео - 0:35:00...0:50:05
+
 /*
  * Array.prototype.filter()
  * - Поэлементо перебирает оригинальный массив;
@@ -6,6 +8,7 @@
  *    - если коллбек вернул "true" элемент добавляется в возвращаемый массив;
  *    - если коллбек вернул "false" элемент НЕ добавляется в возвращаемый массив.
  */
+console.groupCollapsed('Array.prototype.filter()'); //! Log group name
 
 // Исходный массив с числами
 const numbers = [5, 10, 15, 20, 25];
@@ -15,13 +18,16 @@ const filteredNumbers = numbers.filter(number => number < 10 || number > 20);
 // Выводим в лог значение переменной "filteredNumbers"
 console.log('Значение переменной "filteredNumbers":');
 console.log(filteredNumbers);
-console.log('******************************************');
 
+console.groupEnd(); //! Log group end
+console.log('-1----------------------------------------------');
 
 
 /*
  * Получаем массив всех онлайн игроков
  */
+console.groupCollapsed('Получаем массив всех онлайн игроков'); //! Log group name
+
 // Пример использования функции-фильтра "filter" для создания нового массива объектов на основе существующего исходного массива объектов - базы данных. Например, из существующего массива объектов нам нужно выбрать только те объекты, у которых свойство "online" -> "true", и из этих объектов создать новый массив.
 
 // Исходный массив объектов - база данных
@@ -50,13 +56,17 @@ const onlinePlayers2 = players.filter(({ online }) => online);
 // Выводим в лог исходный массив объектов "onlinePlayers2"
 console.log('Новый массив "onlinePlayers2":');
 console.table(onlinePlayers2);
-console.log('******************************************');
 
+
+console.groupEnd(); //! Log group end
+console.log('-2----------------------------------------------');
 
 
 /*
  * Получаем массив всех оффлайн игроков
  */
+console.groupCollapsed('Получаем массив всех оффлайн игроков'); //! Log group name
+
 // Продолжение предыдущего примера, но с обратным условием фильтрации
 
 // Функция-фильтр "filter" для выбора объектов, у которых свойство "online" -> "false".
@@ -64,13 +74,16 @@ const offlinePlayers = players.filter(player => !player.online);
 // Выводим в лог исходный массив объектов "offlinePlayers"
 console.log('Новый массив "offlinePlayers":');
 console.table(offlinePlayers);
-console.log('******************************************');
 
+console.groupEnd(); //! Log group end
+console.log('-3----------------------------------------------');
 
 
 /*
  * Получаем список "хардкорных" игроков, у которых время проведенное в игре - больше 250.
  */
+console.groupCollapsed('Получаем список "хардкорных" игроков'); //! Log group name
+
 // Продолжение предыдущего примера, но с другим условием "фильтрации". В этом варианте необходимо выбрать "играков", у которых показатель времени проведенного в угре - превышает показатель в "250" единиц.
 
 // Функция-фильтр "filter" для выбора объектов, у которых свойство "timePlayed" > "250".
@@ -89,3 +102,6 @@ const hardcorePlayers2 = players.filter(player => player.timePlayed > 2500);
 // Выводим в лог исходный массив объектов "hardcorePlayers"
 console.log('Новый массив "hardcorePlayers2":');
 console.table(hardcorePlayers2);
+
+console.groupEnd(); //! Log group end
+console.log('-4----------------------------------------------');
